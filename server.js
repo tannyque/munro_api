3c1e2663fb10defacc89445e234a80a3987a8dd4
johneas10 <easton3003@gmail.com>
2016-10-09 11:19:52 +0100

    server runs & data is recieved

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var path = require('path');
+var path = require("path");
+var fs = require("fs");
+var MUNROS_JSON = path.join(__dirname, "data/munros.json");
+
+app.use(express.static("public"));
+
+app.set("port", (process.env.PORT || 3003));
+
+app.listen(app.get("port"), function() {
+  console.log("Node app is running on port", app.get("port"));
+});
+
+app.get("/", function(req, res){
+  res.sendfile("public/index.html");
+});
+
+app.get("/api/munros", function(req, res){
+  fs.readFile(MUNROS_JSON, function(err, data){
+    if(err) process.exit(1);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(JSON.parse(data));
+  });
+});
